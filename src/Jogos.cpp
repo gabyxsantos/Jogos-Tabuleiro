296cef195f3ee9a8699fb9712bce5104ecbe6bce
root <root@DESKTOP-9JRSHML.>
2024-12-31 14:50:01 -0300

    adicionando o jogo Reversi

--- src/Jogos.cpp
+++ src/Jogos.cpp
@@ -412,17 +412,326 @@ bool Jogo_Da_Velha::testar_vitoria(const std::string& peca){
 };
 
 //Funções da classe Reversi:
-Reversi::Reversi(){};
-
+Reversi::Reversi(){
+    definir_tamanho_tabuleiro(8,8);
+    inicializar_tabuleiro();
+};
+void Reversi::colocar_pecas_iniciais(std::string jogador1, std::string jogador2){
+    this->tabuleiro[3][3] = jogador1;
+    this->tabuleiro[4][4] = jogador1;
+    this->tabuleiro[4][3] = jogador2;
+    this->tabuleiro[3][4] = jogador2;
+}
 bool Reversi::verificar_jogada(int linha, int coluna){
+    if (coluna < 1 || coluna > 8){
+        imprimir_erro("Coluna inválida!");
+        return false;
+    }
+    if (linha < 1 || linha > 8){
+        imprimir_erro("Linha inválida!");
+        return false;
+    }
+    if (tabuleiro[linha - 1][coluna - 1].compare(PECA_O) != 0){
+        imprimir_erro("Movimento inválido!");
+        return false;
+    }
     return true;
 };
-void Reversi::ler_jogada(const std::string& peca){};
-bool Reversi::testar_vitoria(const std::string& peca){
-    return true;
+void Reversi::ler_jogada(const std::string& peca){
+    int coluna, linha;
+    while (true){
+        std::cout << "Escolha uma coluna (1-" << colunas << ") para colocar a peça " << peca << ": ";
+        std::cin >> coluna;
+        std::cout << "Escolha uma linha (1-" << linhas << ") para colocar a peça " << peca << ": ";
+        std::cin >> linha;
+            if (verificar_jogada(linha,coluna)) {
+                tabuleiro[linha - 1][coluna - 1] = peca;
+                Reversi::linha_ultima_jogada = (linha-1);
+                Reversi::coluna_ultima_jogada = (coluna-1);
+                return;
+            }
+    }
 };
-void Reversi::mostrar_posicoes_possiveis(){};
-void Reversi::converter_pecas(){};
+bool Reversi::testar_vitoria(const std::string& peca_1){
+    int casas_jogador_1=0;
+    int casas_vazias = 0;
+    for (const auto& linha : tabuleiro) {
+        for (const auto& casa : linha) {
+            if (casa == peca_1) {
+                casas_jogador_1++;
+            }
+            if (casa == "  ") {
+                casas_vazias++;
+            }
+        }
+    }
+    if (casas_jogador_1 > (64-(casas_jogador_1+casas_vazias))) return true;
+    else return false;
+};
+void Reversi::mostrar_posicoes_possiveis(std::string jogador1, std::string jogador2){
+    for (int i = 0; i < 8; ++i){ //retira as posições do outro jogador
+        for (int j = 0; j < 8; ++j){
+            if (tabuleiro[i][j].compare(PECA_O) == 0) {
+                tabuleiro[i][j] = "  ";
+            }
+        }
+    }
+    //Conferir linhas
+    for (int i = 0; i < 8; ++i){ //itera pela esquerda
+        for (int j = 0; j < 6; ++j){
+            if (tabuleiro[i][j].compare(jogador1) == 0 && tabuleiro[i][j+1].compare(jogador2) == 0){
+                while (tabuleiro[i][j+1].compare(jogador2) == 0 && j+2<8)
+                {
+                    ++j; 
+                    if (tabuleiro[i][j+1].compare("  ") == 0) {
+                                tabuleiro[i][j+1] = PECA_O;
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < 8; ++i){ //itera pela direita
+        for (int j = 7; j > 1; --j){
+            if (tabuleiro[i][j].compare(jogador1) == 0 && tabuleiro[i][j-1].compare(jogador2) == 0){
+                while (tabuleiro[i][j-1].compare(jogador2) == 0 && j-2>=0)
+                {
+                    --j; 
+                    if (tabuleiro[i][j-1].compare("  ") == 0) {
+                                tabuleiro[i][j-1] = PECA_O;
+                    }
+                }
+            }
+        }
+    }
+    //Conferir colunas
+    for (int j = 0; j < 8; ++j){ //itera de cima para baixo
+        for (int i = 0; i < 6; ++i){
+            if (tabuleiro[i][j].compare(jogador1) == 0 && tabuleiro[i+1][j].compare(jogador2) == 0){
+                while (tabuleiro[i+1][j].compare(jogador2) == 0 && i+2<8)
+                {
+                    ++i; 
+                    if (tabuleiro[i+1][j].compare("  ") == 0) {
+                                tabuleiro[i+1][j] = PECA_O;
+                    }
+                }
+            }
+        }
+    }
+    for (int j = 0; j < 8; ++j){ //itera de baixo para cima
+        for (int i = 7; i > 1; --i){
+            if (tabuleiro[i][j].compare(jogador1) == 0 && tabuleiro[i-1][j].compare(jogador2) == 0){
+                while (tabuleiro[i-1][j].compare(jogador2) == 0 && i-2>=0)
+                {
+                    --i; 
+                    if (tabuleiro[i-1][j].compare("  ") == 0) {
+                                tabuleiro[i-1][j] = PECA_O;
+                    }
+                }
+            }
+        }
+    }
+
+    //Conferir diagonais
+    for (int i = 2; i < 8; ++i){ //diagonais inferior esquerda à superior direita:
+        for (int j = 0; j < 6; ++j){
+            if (tabuleiro[i][j].compare(jogador1) == 0 && tabuleiro[i-1][j+1].compare(jogador2) == 0){
+                int confere_linha_diag = i;
+                int confere_coluna_diag = j;
+                while (tabuleiro[confere_linha_diag-1][confere_coluna_diag+1].compare(jogador2) == 0 && confere_linha_diag-2>=0 && confere_coluna_diag+2<8 )
+                {
+                    --confere_linha_diag;
+                    ++confere_coluna_diag;
+                    if (tabuleiro[confere_linha_diag-1][confere_coluna_diag+1].compare("  ") == 0) {
+                                tabuleiro[confere_linha_diag-1][confere_coluna_diag+1] = PECA_O;
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 2; i < 8; ++i){ //diagonais inferior direita à superior esquerda:
+        for (int j = 7; j >= 0; --j){
+            if (tabuleiro[i][j].compare(jogador1) == 0 && tabuleiro[i-1][j-1].compare(jogador2) == 0){
+                int confere_linha_diag = i;
+                int confere_coluna_diag = j;
+                while (tabuleiro[confere_linha_diag-1][confere_coluna_diag-1].compare(jogador2) == 0 && confere_linha_diag-2>=0 && confere_coluna_diag-2>=0)
+                {
+                    --confere_linha_diag;
+                    --confere_coluna_diag;
+                    if (tabuleiro[confere_linha_diag-1][confere_coluna_diag-1].compare("  ") == 0) {
+                                tabuleiro[confere_linha_diag-1][confere_coluna_diag-1] = PECA_O;
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i <6; ++i){ //diagonais superior direita à inferior esquerda:
+        for (int j = 7; j > 1; --j){
+            if (tabuleiro[i][j].compare(jogador1) == 0 && tabuleiro[i+1][j-1].compare(jogador2) == 0){
+                int confere_linha_diag = i;
+                int confere_coluna_diag = j;
+                while (tabuleiro[confere_linha_diag+1][confere_coluna_diag-1].compare(jogador2) == 0 && confere_linha_diag+2<8 && confere_coluna_diag-2>=0)
+                {
+                    ++confere_linha_diag;
+                    --confere_coluna_diag;
+                    if (tabuleiro[confere_linha_diag+1][confere_coluna_diag-1].compare("  ") == 0) {
+                                tabuleiro[confere_linha_diag+1][confere_coluna_diag-1] = PECA_O;
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < 6; ++i){ //diagonais superior esquerda à inferior direita:
+        for (int j = 0; j < 6; ++j){
+            if (tabuleiro[i][j].compare(jogador1) == 0 && tabuleiro[i+1][j+1].compare(jogador2) == 0){
+                int confere_linha_diag = i;
+                int confere_coluna_diag = j;
+                while (tabuleiro[confere_linha_diag+1][confere_coluna_diag+1].compare(jogador2) == 0 && confere_linha_diag+2<8 && confere_coluna_diag+2<8 ) 
+                {
+                    ++confere_linha_diag;
+                    ++confere_coluna_diag;
+                    if (tabuleiro[confere_linha_diag+1][confere_coluna_diag+1].compare("  ") == 0) {
+                                tabuleiro[confere_linha_diag+1][confere_coluna_diag+1] = PECA_O;
+                    }
+                }
+            }
+        }
+    }
+};
+void Reversi::converter_pecas(std::string& peca_nova, std::string& peca_convertida){
+    //CONFERINDO AS POSIÇÕES ABAIXO:
+    if (linha_ultima_jogada<6){
+        if (tabuleiro[linha_ultima_jogada+1][coluna_ultima_jogada].compare(peca_convertida) == 0){
+            int i=linha_ultima_jogada;
+            while (i<7 && tabuleiro[i+1][coluna_ultima_jogada].compare(peca_convertida) == 0){
+                    ++i; 
+            }
+            if (i<7 && tabuleiro[i+1][coluna_ultima_jogada].compare(peca_nova) == 0){
+                while (tabuleiro[i][coluna_ultima_jogada].compare(peca_convertida) == 0){
+                    tabuleiro[i][coluna_ultima_jogada] = peca_nova;
+                    --i; 
+                }
+            }
+        }
+    }
+    //CONFERINDO AS POSIÇÕES ACIMA:
+    if (linha_ultima_jogada>1){
+        if (tabuleiro[linha_ultima_jogada-1][coluna_ultima_jogada].compare(peca_convertida) == 0){
+            int i=linha_ultima_jogada;
+            while (i>0 && tabuleiro[i-1][coluna_ultima_jogada].compare(peca_convertida) == 0){
+                    --i; 
+            }
+            if (i>0 && tabuleiro[i-1][coluna_ultima_jogada].compare(peca_nova) == 0){
+                while (tabuleiro[i][coluna_ultima_jogada].compare(peca_convertida) == 0){
+                    tabuleiro[i][coluna_ultima_jogada] = peca_nova;
+                    ++i; 
+                }
+            }
+        }
+    }
+    //CONFERINDO AS POSIÇÕES À ESQUERDA:
+    if (coluna_ultima_jogada>1){
+        if (tabuleiro[linha_ultima_jogada][coluna_ultima_jogada-1].compare(peca_convertida) == 0){
+            int i=coluna_ultima_jogada;
+            while (i>0 && tabuleiro[linha_ultima_jogada][i-1].compare(peca_convertida) == 0){
+                    --i; 
+            }
+            if (i>0 && tabuleiro[linha_ultima_jogada][i-1].compare(peca_nova) == 0){
+                while (tabuleiro[linha_ultima_jogada][i].compare(peca_convertida) == 0){
+                    tabuleiro[linha_ultima_jogada][i] = peca_nova;
+                    ++i; 
+                }
+            }
+        }
+    }
+    //CONFERINDO AS POSIÇÕES À DIREITA:
+    if (coluna_ultima_jogada<6){
+        if (tabuleiro[linha_ultima_jogada][coluna_ultima_jogada+1].compare(peca_convertida) == 0){
+            int i=coluna_ultima_jogada;
+            while (i<7 && tabuleiro[linha_ultima_jogada][i+1].compare(peca_convertida) == 0){
+                    ++i; 
+            }
+            if (i<7 && tabuleiro[linha_ultima_jogada][i+1].compare(peca_nova) == 0){
+                while (tabuleiro[linha_ultima_jogada][i].compare(peca_convertida) == 0){
+                    tabuleiro[linha_ultima_jogada][i] = peca_nova;
+                    --i; 
+                }
+            }
+        }
+    } 
+    //CONFERINDO A DIAGONAL INFERIOR-ESQUERDO A SUPERIOR-DIREITO:
+    if (coluna_ultima_jogada<6 && linha_ultima_jogada >1){ //col utl mais ou menos 1
+        if (tabuleiro[linha_ultima_jogada-1][coluna_ultima_jogada+1].compare(peca_convertida) == 0){
+            int i=linha_ultima_jogada;
+            int j=coluna_ultima_jogada;
+            while (i>0 && j<7 && tabuleiro[i-1][j+1].compare(peca_convertida) == 0){
+                    --i;
+                    ++j; 
+            }
+            if (i>0 && j<7 && tabuleiro[i-1][j+1].compare(peca_nova) == 0){
+                while (tabuleiro[i][j].compare(peca_convertida) == 0){
+                    tabuleiro[i][j] = peca_nova;
+                    ++i; 
+                    --j;
+                }
+            }
+        }
+    }
+    //CONFERINDO A DIAGONAL SUPERIOR-DIREITO A INFERIOR-ESQUERDO:
+    if (coluna_ultima_jogada>1 && linha_ultima_jogada <6){ //col utl mais ou menos 1
+        if (tabuleiro[linha_ultima_jogada+1][coluna_ultima_jogada-1].compare(peca_convertida) == 0){
+            int i=linha_ultima_jogada;
+            int j=coluna_ultima_jogada;
+            while (i<7 && j>0 && tabuleiro[i+1][j-1].compare(peca_convertida) == 0){
+                    ++i;
+                    --j; 
+            }
+            if (i<7 && j>0 && tabuleiro[i+1][j-1].compare(peca_nova) == 0){
+                while (tabuleiro[i][j].compare(peca_convertida) == 0){
+                    tabuleiro[i][j] = peca_nova;
+                    --i; 
+                    ++j;
+                }
+            }
+        }
+    }
+    //CONFERINDO A DIAGONAL SUPERIOR-ESQUERDO A INFERIOR-DIREITO:
+    if (coluna_ultima_jogada<6 && linha_ultima_jogada <6){ 
+        if (tabuleiro[linha_ultima_jogada+1][coluna_ultima_jogada+1].compare(peca_convertida) == 0){
+            int i=linha_ultima_jogada;
+            int j=coluna_ultima_jogada;
+            while (i<7 && j<7 && tabuleiro[i+1][j+1].compare(peca_convertida) == 0){
+                    ++i;
+                    ++j; 
+            }
+            if (i<7 && j<7 && tabuleiro[i+1][j+1].compare(peca_nova) == 0){
+                while (tabuleiro[i][j].compare(peca_convertida) == 0){
+                    tabuleiro[i][j] = peca_nova;
+                    --i; 
+                    --j;
+                }
+            }
+        }
+    }
+    //CONFERINDO A DIAGONAL SUPERIOR-DIREITO A INFERIOR-ESQUERDO:
+        if (coluna_ultima_jogada>1 && linha_ultima_jogada >1){ 
+        if (tabuleiro[linha_ultima_jogada-1][coluna_ultima_jogada-1].compare(peca_convertida) == 0){
+            int i=linha_ultima_jogada;
+            int j=coluna_ultima_jogada;
+            while (i>0 && j>0 && tabuleiro[i-1][j-1].compare(peca_convertida) == 0){
+                    --i;
+                    --j; 
+            }
+            if (i>0 && j>0 && tabuleiro[i-1][j-1].compare(peca_nova) == 0){
+                while (tabuleiro[i][j].compare(peca_convertida) == 0){
+                    tabuleiro[i][j] = peca_nova;
+                    ++i; 
+                    ++j;
+                }
+            }
+        }
+    }
+}; 
+
 
 //Funções da classe Tutorial:
 void Tutorial::iniciar_tutorial_lig4(){};
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main() {
     
     /*
-    // Teste de implementação para Lig 4
+    // Teste de implementação para Lig 4 /////////////////////////////////////////////////////
     Lig_4 jogo;
 
     std::string jogador1, jogador2;
@@ -66,7 +66,7 @@ int main() {
     */
 
     /*
-    // Teste de implementação para Jogo da Velha
+    // Teste de implementação para Jogo da Velha ///////////////////////////////////////////
     Jogo_Da_Velha jogo;
 
     std::string jogador1, jogador2;
@@ -125,5 +125,79 @@ int main() {
     if (empate) jogo.finalizar_partida_empate();
     else jogo.finalizar_partida_vencedor(nome_atual);
     */
+
+
+   // Teste de implementação para Reversi /////////////////////////////////////////////////////
+    Reversi jogo;
+
+    std::string jogador1, jogador2;
+    std::string peca1, peca2;
+
+    // Obter os nomes dos jogadores
+    std::cout << "Jogador 1, insira seu apelido:" << std::endl;
+    std::cin >> jogador1;
+    peca1 = jogo.definir_cor(" "); // O primeiro jogador escolhe a cor
+
+    std::cout << "Jogador 2, insira seu apelido:" << std::endl;
+    std::cin >> jogador2;
+    peca2 = jogo.definir_cor(peca1); // O segundo jogador escolhe a cor diferente do primeiro
+
+    //std::string nome_atual = jogador1;
+    //std::string jogador_atual = peca1;
+
+    // Inicializar o tabuleiro
+    jogo.inicializar_tabuleiro();
+    jogo.colocar_pecas_iniciais(peca1, peca2);
+
+        int i=0;
+        while(i < 28){
+            jogo.mostrar_posicoes_possiveis(peca1, peca2);
+            jogo.imprimir_tabuleiro();
+            jogo.ler_jogada(peca1);
+            std::cout << "ANTES DE CONVERTER "<< std::flush;
+            jogo.converter_pecas(peca1, peca2);
+            jogo.mostrar_posicoes_possiveis(peca2, peca1);
+            jogo.imprimir_tabuleiro();
+            jogo.ler_jogada(peca2);
+            jogo.converter_pecas(peca2, peca1);
+            i++;
+        }
+        if(jogo.testar_vitoria(peca1)) std::cout << "O Jogador 1 ganhou!";
+        if(jogo.testar_vitoria(peca2)) std::cout << "O Jogador 2 ganhou!";
+
+        // Informar o turno atual
+        //std::cout << nome_atual << " (" << jogador_atual << "), é sua vez!" << std::endl;
+
+        // Ler a jogada do jogador atual
+        //jogo.ler_jogada(jogador_atual);
+
+        // Verificar vitória
+        //if (jogo.testar_vitoria(jogador_atual)) {
+            //terminou = true;
+            //break;
+        //}
+
+        // Verificar empate
+        //if (jogo.testar_empate(peca1, peca2)) {
+            //terminou = true;
+            //empate = true;
+            //break;
+        //}
+
+        // Trocar de turno
+        //if (jogador_atual == peca1) {
+        //    jogador_atual = peca2;
+        //    nome_atual = jogador2;
+        //} else {
+        //    jogador_atual = peca1;
+        //    nome_atual = jogador1;
+        //}
+    //}
+
+    // Finalizar a partida
+    //if (empate) jogo.finalizar_partida_empate();
+    //else jogo.finalizar_partida_vencedor(nome_atual);
+
+
     return 0;
 }

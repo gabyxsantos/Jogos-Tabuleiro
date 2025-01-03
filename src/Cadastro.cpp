509f9f439052e9a2bf14277b6b56d806c1a3a30f
Carolina Barcellos <carolinapbarcellos@gmail.com>
2025-01-03 14:28:18 -0300

    Melhoria na ordenação dos jogadores para evitar erros

--- src/Cadastro.cpp
+++ src/Cadastro.cpp
@@ -47,12 +47,20 @@
 //Funções da classe Registro_jogadores
     void Registro_jogadores::ordenar_jogadores_nome(){ // Ordenar por nome
         Registro_jogadores::Jogadores.sort([](Jogador& a, Jogador& b) {
-        return a.get_nome() < b.get_nome();
+            std::string nome_a=a.get_nome();
+            std::string nome_b=b.get_nome();
+            std::transform(nome_a.begin(), nome_a.end(), nome_a.begin(), ::tolower);
+            std::transform(nome_b.begin(), nome_b.end(), nome_b.begin(), ::tolower);
+            return nome_a < nome_b;
         });
     };
     void Registro_jogadores::ordenar_jogadores_apelido(){ // Ordenar por apelido
         Registro_jogadores::Jogadores.sort([](Jogador& a, Jogador& b) {
-            return a.get_apelido() < b.get_apelido();
+            std::string apelido_a=a.get_apelido();
+            std::string apelido_b=b.get_apelido();
+            std::transform(apelido_a.begin(), apelido_a.end(), apelido_a.begin(), ::tolower);
+            std::transform(apelido_b.begin(), apelido_b.end(), apelido_b.begin(), ::tolower);
+            return apelido_a < apelido_b;
         });
     };
     std::list<Jogador>::iterator Registro_jogadores::buscar_jogador(std::string& apelido){

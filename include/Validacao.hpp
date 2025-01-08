40ba6fb5a461167a1233dcd9aa280d83f7a0f119
Carolina Barcellos <carolinapbarcellos@gmail.com>
2025-01-07 21:39:46 -0300

    Criação e modularização da classe Validacao

---
+++ include/Validacao.hpp
@@ -0,0 +1,65 @@
+#ifndef VALIDACAO_HPP
+#define VALIDACAO_HPP
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+class Validacao{
+    private:
+        
+        //funcao para limpar o estado de erro e descartar a entrada invalida
+        void limpar_entrada();
+        bool validar_char(char& valor);
+        bool validar_string(const std::string& valor);
+
+        // Funções templadas para entrada de dados
+        template <typename T>
+        inline bool validar_entrada(T& valor) { //usando inline para evitar múltiplas definições ao incluir o hpp em diferentes arquivos cpp
+            if (std::cin >> valor) {
+                return true; // Entrada válida
+            }
+            
+            Validacao::limpar_entrada();
+            return false; // Entrada inválida
+        }
+
+        template <>
+        inline bool validar_entrada(char& valor) {
+            if (std::cin >> valor) {
+                if (Validacao::validar_char(valor)) {
+                    return true; // Validação de char passou
+                }
+            }
+            
+            Validacao::limpar_entrada();
+            return false; // Entrada inválida
+        }
+
+        template <>
+        inline bool validar_entrada(std::string& valor) {
+            if (std::cin >> valor) {
+                if (Validacao::validar_string(valor)) {
+                    return true; // Validação de string passou
+                }
+            }
+            // Limpa o estado de erro e descarta a entrada inválida
+            Validacao::limpar_entrada();
+            return false; // Entrada inválida
+        }
+        
+    public:
+
+        void imprimir_erro(const std::string& erro);
+
+        template <typename T>
+        void pedir_usuario(T& valor) {
+            while (!validar_entrada(valor)) {
+                imprimir_erro("Entrada inválida. Insira no formato correto!");
+            }
+        }
+    
+};
+
+
+#endif
